7e2b69eb6aad48893865cf7d64b2188ced9fa89c
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2021-09-07 20:03:30 +0300

    high-score component

--- Algorithms/exercism.php
+++ Algorithms/exercism.php
@@ -26,15 +26,9 @@ helloWorld();
 
 /*
 ---------------------------------------------------------------------------
-The classical introductory exercise. Just say "Hello, World!".
+Reverse a string
 
-"Hello, World!" is the traditional first program for beginning programming in a new language or environment.
-
-The objectives are simple:
-
-Write a function that returns the string "Hello, World!".
-Run the test suite and make sure that it succeeds.
-Submit your solution and check it at the website.
+For example: input: "cool" output: "looc"
 ----------------------------------------------------------------------------
 */
 
@@ -43,4 +37,42 @@ function reverseString(string $text): String{
     return strrev($text);
 }
 reverseString("cool");
+
+/*
+---------------------------------------------------------------------------
+Your task is to build a high-score component of the classic Frogger game, 
+one of the highest selling and addictive games of all time,and a classic of the arcade era.
+Your task is to write methods that return the highest score from the list,
+the last added score and the three highest scores.
+----------------------------------------------------------------------------
+*/
+
+class HighScores{
+  
+public array $scores = [];
+public mixed $latest;
+public mixed $personalBest;
+public array $personalTopThree;
+  
+public function __construct(array $scores):void{
+  
+ $this->scores = $scores;
+}
+public function latest():mixed{
+  
+$this->latest = end($this->scores);
+  
+}
+public function personalBest():mixed{
+  
+$this->personalBest =  max($this->scores);
+  
+}
+public function personalTopThree():array{
+  
+ rsort($this->scores);
+ return array_splice($this->scores, 0, 2);
+  
+}
+}
 

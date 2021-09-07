579a6025bee869e3e0d9f89d4dbd0480f0d396b3
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2021-09-07 19:55:20 +0300

    Hello, World

--- Algorithms/exercism.php
+++ Algorithms/exercism.php
@@ -2,3 +2,25 @@
 //This file contains all my tests and exercises done at https://exercism.org/tracks/php/
 //this is just for reference for future me 
 
+
+/*
+---------------------------------------------------------------------------
+The classical introductory exercise. Just say "Hello, World!".
+
+"Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+
+The objectives are simple:
+
+Write a function that returns the string "Hello, World!".
+Run the test suite and make sure that it succeeds.
+Submit your solution and check it at the website.
+----------------------------------------------------------------------------
+*/
+
+
+function helloWorld(){
+  
+    return "Hello, World!";
+}
+helloWorld();
+

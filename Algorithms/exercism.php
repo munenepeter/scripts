86a1a22ce95245e20ceefff8da99698f49cfde9c
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2021-09-07 19:58:38 +0300

    reverseString

--- Algorithms/exercism.php
+++ Algorithms/exercism.php
@@ -18,9 +18,29 @@ Submit your solution and check it at the website.
 */
 
 
-function helloWorld(){
+function helloWorld():String{
   
     return "Hello, World!";
 }
 helloWorld();
+
+/*
+---------------------------------------------------------------------------
+The classical introductory exercise. Just say "Hello, World!".
+
+"Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+
+The objectives are simple:
+
+Write a function that returns the string "Hello, World!".
+Run the test suite and make sure that it succeeds.
+Submit your solution and check it at the website.
+----------------------------------------------------------------------------
+*/
+
+function reverseString(string $text): String{
+  
+    return strrev($text);
+}
+reverseString("cool");
 

0676ea8ede16b6bf35bac7f411b4372abfae34f5
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-01-11 11:29:43 +0300

    update

--- snippets/exercism.php
+++ snippets/exercism.php
@@ -44,6 +44,7 @@ Your task is to build a high-score component of the classic Frogger game,
 one of the highest selling and addictive games of all time,and a classic of the arcade era.
 Your task is to write methods that return the highest score from the list,
 the last added score and the three highest scores.
+updated at 11.01.2022
 ----------------------------------------------------------------------------
 */
 

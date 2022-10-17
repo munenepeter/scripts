c0534b6210ce5805edfbc34f72c9b6820d6e1ab8
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-10-17 16:50:40 +0300

    add convert keywords to pearl

---
+++ snippets/convert_word_to_pearl.php
@@ -0,0 +1,42 @@
+<?php
+
+function getRandColor() {
+    $rgbColor = [];
+    foreach (['r', 'g', 'b'] as $color) {
+        //Generate a random number between 0 and 255.
+        $rgbColor[$color] = mt_rand(0, 255);
+    }
+    $colorCode = implode(",", $rgbColor);
+    return "rgb($colorCode)";
+}
+
+//change to file
+
+$text = trim(file_get_contents("pearls.txt"));
+
+
+
+$kw = explode(",", $text);
+
+
+
+$kw = array_map(function ($v) {
+    return trim($v);
+}, $kw);
+
+$kq = array_unique($kw);
+
+$n = [];
+
+foreach ($kq as $key => $value) {
+    $n[] = [
+        'word' => $value,
+        'color' => getRandColor()
+    ];
+}
+
+//$n = array_unique($n);
+
+foreach ($n as $key) {
+    file_put_contents("kw.txt", json_encode($key). PHP_EOL, FILE_APPEND | LOCK_EX);
+}

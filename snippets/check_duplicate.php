79513503869b550dd649ba55617e0c75cc70caff
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-10-17 16:56:06 +0300

    update download

---
+++ snippets/check_duplicate.php
@@ -0,0 +1,16 @@
+<?php
+
+$i = 0;
+while ($i < 100000) {
+
+    $str[] = uniqid();
+    echo $str[$i]. " index => $i". PHP_EOL ;
+
+
+    if (count(array_unique($str)) < count($str)) {
+        echo "We've a duplicate of " . $str[$i] . " with only ". $i+1 ." strings";
+        break;
+    }
+
+    $i++;
+}
---
+++ snippets/download-file.php
@@ -0,0 +1,21 @@
+<?php
+    //
+
+    function downloadFile($dir, $file) {
+
+        if(file_exists($file."uuj")) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.basename($file).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            flush(); // Flush system output buffer
+            readfile($dir.$file);
+            die();
+        } else {
+            http_response_code(404);
+            die();
+        }
+    }

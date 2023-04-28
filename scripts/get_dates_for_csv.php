641e1a10b2f14e69869ff7598c80a775db9804a2
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-04-28 16:00:11 +0300

    exclude weekends

--- scripts/get_dates_for_csv.php
+++ scripts/get_dates_for_csv.php
@@ -1,9 +1,14 @@
 <?php
 /**
- * This script generates a schedule file for a given month and year,
+ * Generates a CSV file with dates for a given month and year.
  * with five entries per day for each of five people.
+ * Usage:
+ * php script.php <month> <year> [-w]
  *
- * Usage: php script.php <month> <year>
+ * Arguments:
+ * - month (int): the month to generate dates for (default is the current month)
+ * - year (int): the year to generate dates for (default is the current year)
+ * - -w (optional): exclude weekends from the generated dates
  */
 
 // Set the timezone
@@ -11,11 +16,17 @@ date_default_timezone_set('UTC');
 
 // Get the month and year from the command line arguments
 if ($argc < 3) {
-    echo "Usage: php script.php <month> <year>\n";
+    echo "Usage: php script.php <month> <year> [-w]\n";
     exit(1);
 }
-$month = (int)$argv[1];
-$year = (int)$argv[2];
+$month = (int)$argv[1] ?? date('m');
+$year = (int)$argv[2] ?? date('Y');
+
+// Check if the -w flag is set
+$excludeWeekends = false;
+if (in_array('-w', $argv)) {
+    $excludeWeekends = true;
+}
 
 // Get the number of days in the month
 $numDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
@@ -24,10 +35,13 @@ $numDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 $file = fopen(__DIR__ . '/dates.csv', 'w');
 
 $first_day = date('d/m/Y', mktime(0, 0, 0, $month, 1, $year));
-
 // Loop through the days of the month
 for ($i = 1; $i <= $numDays; $i++) {
-
+    // Skip weekends if the flag is set
+    if ($excludeWeekends && (date('N', mktime(0, 0, 0, $month, $i, $year)) >= 6)) {
+        continue;
+    }
+   
     // Loop five times to output the same day
     for ($j = 1; $j <= 5; $j++) {
 
@@ -48,6 +62,3 @@ for ($i = 1; $i <= $numDays; $i++) {
 fclose($file);
 
 echo "Output written to dates.csv\n";
-
-
-

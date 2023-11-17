90844b1fded8a2b10b2945f82ad6acf86da569f9
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-11-17 16:09:57 +0300

    add better comments

--- create-laravel-project.sh
+++ create-laravel-project.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/bash
+#---------------------------------------------------------------------------------
+# A Bash script that creates a new laravel app in the specified folder
+# and installs a TALL stack preset (Livewire, Tailwind, & Alpine) - https://github.com/laravel-frontend-presets/tall
+# Since most apps will have auth, we will install the auth preset
+#---------------------------------------------------------------------------------
+
+#!/usr/bin/bash/env bash
 
 # bash -c "$(curl -sS https://raw.githubusercontent.com/munenepeter/scripts/main/create-laravel-project.sh)" my_laravel_project
 
@@ -7,6 +13,8 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <project_name>"
     exit 1
 fi
+
+echo  "Hi, the script will create a new laravel project in $project_name and install a TALL preset..."
 
 # Set the project name from the command line argument
 project_name=$1

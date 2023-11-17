5d2e4dc522459ed994e5d73b07e4b336287b35f6
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-11-17 15:47:12 +0300

    Create create_new_laravel.sh

---
+++ create_new_laravel.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <project_name>"
+    exit 1
+fi
+
+# Set the project name from the command line argument
+project_name=$1
+project_path=$(pwd)/$project_name
+
+# Install Laravel
+composer create-project --prefer-dist laravel/laravel $project_name
+
+# Change directory to the project folder
+cd $project_path
+
+# Run additional commands
+# Install a TALL preset
+composer require livewire/livewire laravel-frontend-presets/tall
+
+# Publish the assets
+php artisan ui tall --auth
+
+# Install front-end dependencies
+npm install
+
+# Build assets
+npm run dev
+
+echo "Laravel project '$project_name' created successfully in '$project_path'."

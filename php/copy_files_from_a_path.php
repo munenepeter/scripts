00c8dfbc0b3a08e2980e5aaeca91d3b829cfb695
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2023-12-29 10:51:09 +0300

    add a script to delete pre-existing files, use for loop

--- php/copy_files_from_a_path.php
+++ php/copy_files_from_a_path.php
@@ -1,15 +1,16 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Move files and directories to script directory
+|--------------------------------------------------------------------------
+|
+| This script is designed to be run exclusively from the command line.
+| It moves files and directories from a specified source folder 
+| to the directory where the script is located.
+|
+*/
 
-/**|
- * |   =================================================
- * |    Move files and directories to script directory
- * |   ===================================================
- * |
- * |    This script is designed to be run exclusively from the command line. It moves files and
- * |    directories from a specified source folder to the directory where the script is located.
- */
-
-$source_folder = '/path/to/your/source/folder';
+$source_folder = __DIR__ . '/../path/to/copy/from/';
 
 /**
  * Recursively move files and directories.
@@ -20,10 +21,11 @@ $source_folder = '/path/to/your/source/folder';
 function move_files(string $source, string $destination) {
     // Remove "." and ".."
     $items = array_diff(scandir($source), ['.', '..']);
+    $itemsCount = count($items);
 
-    foreach ($items as $item) {
-        $source_path = $source . '/' . $item;
-        $destination_path = $destination . '/' . $item;
+    for ($i = 2; $i < $itemsCount; ++$i) {
+        $source_path = $source . '/' . $items[$i];
+        $destination_path = $destination . '/' . $items[$i];
 
         if (is_dir($source_path)) {
             // it's a dir, create the dest dir
@@ -32,16 +34,20 @@ function move_files(string $source, string $destination) {
             }
             move_files($source_path, $destination_path);
         } else {
-            // it's a file, copy
             if (copy($source_path, $destination_path)) {
-                // Uncomment the line below to delete the source file after copying
-                // unlink($source_path);
-                echo "Copied: $item\n";
+                printf("Copied: %s\n", $items[$i]);
             } else {
-                echo "Error copying $item\n";
+                printf("Error copying %s\n", $items[$i]);
             }
         }
     }
 }
+
+//remove all pre-existing files first
+ shell_exec('rm -rf $(ls | grep -v mv.php)');
+// if() === false){
+//     printf("Could not remove previous files in %s", $source_folder);
+//     exit;
+// }
 
 move_files($source_folder, __DIR__);

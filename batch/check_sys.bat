33f1f6cb9ef4095d3f0e6b008650ec0796c306ee
ADMIN <isaacvwarui@gmail.com>
2025-03-27 12:25:55 +0300

    updated check_sys.bat batch script

--- batch/check_sys.bat
+++ batch/check_sys.bat
@@ -1,66 +1,65 @@
 @echo off
 
-echo Checking System...
-
+:: Define report file
 set report_file=device_report.txt
 
-echo === Device Report ===>> %report_file%
-echo.
+echo Checking System...
+echo === Device Report === > %report_file%
+echo. >> %report_file%
 
+:: Checking keyboard
 echo Checking keyboard...
-echo === Keyboard ===>> %report_file%
-wmic path win32_keyboard get DeviceID, Name, NumberOfFunctionKeys, Status, StatusInfo, Description | more>> %report_file%
-echo.>> %report_file%
+echo === Keyboard === >> %report_file%
+powershell -command "Get-CimInstance Win32_Keyboard | Format-Table DeviceID, Name, NumberOfFunctionKeys, Status, Description -AutoSize" >> %report_file%
+echo. >> %report_file%
 
+:: Checking mouse
 echo Checking mouse...
-echo === Mouse ===>> %report_file%
-for /f "skip=1 tokens=2" %%a in ('wmic path win32_pointingdevice get status') do set "mouse_status=%%a"echo Mouse: %mouse_status%
-echo Mouse: %mouse_status% >> %report_file%
-echo.>> %report_file%
+echo === Mouse === >> %report_file%
+powershell -command "$mouse_status = (Get-CimInstance Win32_PointingDevice).Status; echo Mouse: $mouse_status" >> %report_file%
+echo. >> %report_file%
 
+:: Checking Display Adapter
 echo Checking Display Adapter...
-echo === Display Adapter ===>> %report_file%
-for /f "skip=1 tokens=1,2,3" %%a in ('wmic path win32_videocontroller get caption^,status^,adapterram /format:table') do (
-  echo %%a  Status: %%b    Adapter RAM: %%c >> %report_file%
-)
-echo.>> %report_file%
+echo === Display Adapter === >> %report_file%
+powershell -command "Get-CimInstance Win32_VideoController | Format-Table Caption, Status, AdapterRAM -AutoSize" >> %report_file%
+echo. >> %report_file%
 
+:: Checking Disk Drive
 echo Checking Disk Drive...
-echo === Disk Drive ===>> %report_file%
-wmic diskdrive get caption, size, status | more>> %report_file%
+echo === Disk Drive === >> %report_file%
+powershell -command "Get-CimInstance Win32_DiskDrive | Format-Table Caption, Size, Status -AutoSize" >> %report_file%
+echo. >> %report_file%
 
-echo.>> %report_file%
-
+:: Checking RAM
 echo Checking RAM...
-echo === RAM ===>> %report_file%
-wmic memorychip get devicelocator, manufacturer, partnumber, serialnumber, capacity, speed, memorytype, formfactor |more >> %report_file%
-echo.>> %report_file%
+echo === RAM === >> %report_file%
+powershell -command "Get-CimInstance Win32_PhysicalMemory | Format-Table DeviceLocator, Manufacturer, PartNumber, SerialNumber, Capacity, Speed, MemoryType, FormFactor -AutoSize" >> %report_file%
+echo. >> %report_file%
 
+:: Checking Network Adapters
 echo Checking Network Adapters...
-echo === Network Adapters ===>> %report_file%
-wmic path Win32_NetworkAdapter get AdapterType, Name, NetConnectionId, NetEnabled, Status, StatusInfo, Manufacturer | more >> %report_file%
-echo.>> %report_file%
+echo === Network Adapters === >> %report_file%
+powershell -command "Get-CimInstance Win32_NetworkAdapter | Format-Table AdapterType, Name, NetConnectionID, NetEnabled, Status, Manufacturer -AutoSize" >> %report_file%
+echo. >> %report_file%
 
-
+:: Checking Sound Devices
 echo Checking Sound Devices...
-echo === Sound Device ===>> %report_file%
-wmic path win32_sounddevice get caption, status | more >> %report_file%
-echo.>> %report_file%
+echo === Sound Device === >> %report_file%
+powershell -command "Get-CimInstance Win32_SoundDevice | Format-Table Caption, Status -AutoSize" >> %report_file%
+echo. >> %report_file%
 
-echo checking Battery
-echo === Battery ===>> %report_file%
-wmic path win32_battery get Availability,   BatteryStatus,   Caption,   Chemistry,   DeviceID,   EstimatedChargeRemaining,  MaxRechargeTime,   Name,   PNPDeviceID, Status,   StatusInfo | more>> %report_file%
-echo.>> %report_file%
+:: Checking Battery
+echo Checking Battery...
+echo === Battery === >> %report_file%
+powershell -command "Get-CimInstance Win32_Battery | Format-Table Availability, BatteryStatus, Caption, Chemistry, DeviceID, EstimatedChargeRemaining, Name, PNPDeviceID, Status -AutoSize" >> %report_file%
+echo. >> %report_file%
 
-
-
+:: Checking Wireless Connections
 echo Checking Wireless Connections...
-echo === Wireless Connections ===>> %report_file%
-netsh wlan show interfaces >> %report_file%
-
-echo.>> %report_file%
-echo.>> %report_file%
-echo.
+echo === Wireless Connections === >> %report_file%
+powershell -command "netsh wlan show interfaces" >> %report_file%
+echo. >> %report_file%
 
 echo Report generated at %date% %time% >> %report_file%
 echo Done! Check %report_file% for the report.

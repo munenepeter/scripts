0a6808021da31c4bd7804262427dddae83cb1cb2
Peter Munene <peternjega7@gmail.com>
2025-06-20 15:35:16 +0300

    wip

--- php/wifi_monitor.php
+++ php/wifi_monitor.php
@@ -92,7 +92,7 @@ function generateHtmlReport($ssid, $status, $speed, $duration, $internetAccess) 
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Wi-Fi Status Report</title>
-        <script src="https:
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body class="bg-gray-100 flex items-center justify-center h-screen">
         <div class="bg-white p-8 rounded-lg shadow-lg text-center">
